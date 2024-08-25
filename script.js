5134298eea67150a20887a05942dc7a3e969bf75
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-08-25 15:33:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-//your code here
+document.addEventListener('DOMContentLoaded', (event) => {
+    const parent = document.getElementById('parent');
+    let dragSrcEl = null;
+
+    function handleDragStart(e) {
+        this.style.opacity = '0.4';
+        dragSrcEl = this;
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/html', this.innerHTML);
+    }
+
+    function handleDragOver(e) {
+        if (e.preventDefault) {
+            e.preventDefault();
+        }
+        e.dataTransfer.dropEffect = 'move';
+        return false;
+    }
+
+    function handleDragEnter(e) {
+        this.classList.add('over');
+    }
+
+    function handleDragLeave(e) {
+        this.classList.remove('over');
+    }
+
+    function handleDrop(e) {
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        }
+
+        if (dragSrcEl != this) {
+            let tempBackground = window.getComputedStyle(this).backgroundImage;
+            this.style.backgroundImage = window.getComputedStyle(dragSrcEl).backgroundImage;
+            dragSrcEl.style.backgroundImage = tempBackground;
+
+            dragSrcEl.innerHTML = this.innerHTML;
+            this.innerHTML = e.dataTransfer.getData('text/html');
+        }
+
+        return false;
+    }
+
+    function handleDragEnd(e) {
+        this.style.opacity = '1';
+        
+        items.forEach(function (item) {
+            item.classList.remove('over');
+        });
+    }
+
+    let items = document.querySelectorAll('#parent .image');
+    items.forEach(function(item) {
+        item.addEventListener('dragstart', handleDragStart, false);
+        item.addEventListener('dragenter', handleDragEnter, false);
+        item.addEventListener('dragover', handleDragOver, false);
+        item.addEventListener('dragleave', handleDragLeave, false);
+        item.addEventListener('drop', handleDrop, false);
+        item.addEventListener('dragend', handleDragEnd, false);
+    });
+});
